01bd50cbf74b5e2ee20c6546c7e41e03044b0147
samwebber11 <samjain15291@gmail.com>
2019-02-15 19:30:15 +0530

    SideDrawer Toggler Added

--- src/components/Navigation/SideDrawer/toggler/toggler.js
+++ src/components/Navigation/SideDrawer/toggler/toggler.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import classes from './toggler.css'
 
 const toggler = (props) => (
-    <div onClick = {props.clicked}>Menu</div>
-)
+    <div onClick = {props.clicked}
+    className = {classes.DrawerToggle}>
+    <div></div>
+    <div></div>
+    <div></div>
+    </div>
+);
 
 export default toggler;
